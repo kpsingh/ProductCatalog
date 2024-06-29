3fc4bbdbf90a3b30ff8d2d552f9abd8426e5b1ed
Krishna Pratap Singh <kpsingh1234@gmail.com>
2024-06-29 14:18:19 +0530

    Controller Advice added

--- src/main/java/com/lld4/productcatalog/controllers/ProductController.java
+++ src/main/java/com/lld4/productcatalog/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.lld4.productcatalog.controllers;
 
 import com.lld4.productcatalog.dtos.CategoryDto;
 import com.lld4.productcatalog.dtos.ProductDto;
+import com.lld4.productcatalog.mappers.ProductMapper;
 import com.lld4.productcatalog.models.Category;
 import com.lld4.productcatalog.models.Product;
 import com.lld4.productcatalog.services.IProductService;
@@ -19,9 +20,11 @@ import java.util.List;
 public class ProductController {
 
     private final IProductService productService;
+    private final ProductMapper productMapper;
 
-    public ProductController(IProductService productService) {
+    public ProductController(IProductService productService, ProductMapper productMapper) {
         this.productService = productService;
+        this.productMapper = productMapper;
     }
 
 
@@ -40,10 +43,10 @@ public class ProductController {
             Product product = productService.getProduct(productId);
             if (product == null) {
                 // no response received thence the body part id null.
-               // return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+                // return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
                 throw new IllegalArgumentException("product id " + productId + " not found");
             }
-            ProductDto productDto = getProductDto(product);
+            ProductDto productDto = productMapper.getProductDtoFromProduct(product);
 
             // the advantage of returning the ResponseEntity is that we can add the status and any other headers as we want but if we simply return the product then those options will not be available to us
             return new ResponseEntity<>(productDto, customHeaders, HttpStatus.OK);
@@ -53,7 +56,6 @@ public class ProductController {
             throw e;
         }
     }
-
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductDto>> getAllProducts() {
@@ -66,7 +68,7 @@ public class ProductController {
 
         List<ProductDto> productDtos = new ArrayList<>();
         for (Product product : products) {
-            ProductDto productDto = getProductDto(product);
+            ProductDto productDto = productMapper.getProductDtoFromProduct(product);
             productDtos.add(productDto);
         }
 
@@ -75,46 +77,14 @@ public class ProductController {
 
     @PostMapping("/products")
     public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
-        Product product = getProductFromDTO(productDto);
+        Product product = productMapper.getProductFromProductDto(productDto);
         productService.createProduct(product);
         return new ResponseEntity<>(productDto, HttpStatus.CREATED);
     }
 
-    private Product getProductFromDTO(ProductDto productDto) {
-        Product product = new Product();
-        product.setId(productDto.getId());
-        product.setName(productDto.getName());
-        product.setDescription(productDto.getDescription());
-        product.setPrice(productDto.getPrice());
-        product.setImageUrl(productDto.getImageUrl());
-        Category category = new Category();
-        category.setName(productDto.getCategory().getName());
-        category.setDescription(productDto.getCategory().getDescription());
-        product.setCategory(category);
-        return product;
-    }
 
     @PutMapping("{id}")
     public ProductDto replaceProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
-        return productDto;
-    }
-
-    private ProductDto getProductDto(Product product) {
-        ProductDto productDto = new ProductDto();
-        productDto.setId(product.getId());
-        productDto.setName(product.getName());
-        productDto.setPrice(product.getPrice());
-        productDto.setDescription(product.getDescription());
-        productDto.setImageUrl(product.getImageUrl());
-
-        if (product.getCategory() != null) {
-            CategoryDto categoryDto = new CategoryDto();
-            categoryDto.setId(product.getCategory().getId());
-            categoryDto.setName(product.getCategory().getName());
-            categoryDto.setDescription(product.getCategory().getDescription());
-            productDto.setCategory(categoryDto);
-        }
-
         return productDto;
     }
 
--- src/main/java/com/lld4/productcatalog/mappers/ProductMapper.java
+++ src/main/java/com/lld4/productcatalog/mappers/ProductMapper.java
@@ -1,24 +1,24 @@
 package com.lld4.productcatalog.mappers;
 
 import com.lld4.productcatalog.dtos.CategoryDto;
-import com.lld4.productcatalog.dtos.FakeStoreProductDto;
 import com.lld4.productcatalog.dtos.ProductDto;
 import com.lld4.productcatalog.models.Category;
 import com.lld4.productcatalog.models.Product;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FakeStoreProductMapper {
+public class ProductMapper {
 
-    public Product getProductFromDto(FakeStoreProductDto fakeStoreProductDto) {
+    public Product getProductFromProductDto(ProductDto productDto) {
         Product product = new Product();
-        product.setId(fakeStoreProductDto.getId());
-        product.setName(fakeStoreProductDto.getTitle());
-        product.setPrice(fakeStoreProductDto.getPrice());
-        product.setDescription(fakeStoreProductDto.getDescription());
-        product.setImageUrl(fakeStoreProductDto.getImage());
+        product.setId(productDto.getId());
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setImageUrl(productDto.getImageUrl());
         Category category = new Category();
-        category.setName(fakeStoreProductDto.getCategory());
+        category.setName(productDto.getCategory().getName());
+        category.setDescription(productDto.getCategory().getDescription());
         product.setCategory(category);
         return product;
     }
@@ -30,6 +30,7 @@ public class FakeStoreProductMapper {
         productDto.setPrice(product.getPrice());
         productDto.setDescription(product.getDescription());
         productDto.setImageUrl(product.getImageUrl());
+
         if (product.getCategory() != null) {
             CategoryDto categoryDto = new CategoryDto();
             categoryDto.setId(product.getCategory().getId());
